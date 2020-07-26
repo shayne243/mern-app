5f63fddf20f05ef6b639410276fb01dac191dcc4
shayne243 <marques0@purdue.edu>
2020-07-26 12:53:36 +0400

    basic routing done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,31 @@
 import React from 'react';
+import { 
+  BrowserRouter as Router, 
+  Route, 
+  Redirect, 
+  Switch } 
+from 'react-router-dom';
+
 import './App.css'
+import Users from './user/pages/Users'
+import NewPlace from './places/pages/NewPlace'
 
 const App = () => {
   return (
-    <h1>
-      Let's Start!
-    </h1>
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <h1>
+            Let's Start!
+          </h1>
+          <Users />
+        </Route>
+        <Route path="/places/new" exact>
+          <NewPlace />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ frontend/src/user/pages/Users.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Users = () => {
+    return (
+        <h2>
+            This is the users page!
+        </h2>
+    );
+}
+
+export default Users;
