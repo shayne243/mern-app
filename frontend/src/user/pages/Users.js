480a646a2e235169e62194720c4458721eda80c6
shayne243 <marques0@purdue.edu>
2020-07-26 13:21:22 +0400

    userslist component complete

--- frontend/src/user/pages/Users.js
+++ frontend/src/user/pages/Users.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React from "react";
+
+import UsersList from "../components/UsersList";
 
 const Users = () => {
-    return (
-        <h2>
-            This is the users page!
-        </h2>
-    );
-}
+  const USERS = [
+    {
+      id: "u1",
+      name: "Shayne",
+      image:
+        "https://i.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U",
+      places: 3,
+    },
+  ];
+  return <UsersList items={ USERS } />;
+};
 
 export default Users;
