b3051a8ff499b15473edf7c6ee5f81f0378df9d1
shayne243 <marques0@purdue.edu>
2020-07-27 03:11:10 +0400

    added custom buttons

--- frontend/src/places/components/PlaceList.js
+++ frontend/src/places/components/PlaceList.js
@@ -5,7 +5,7 @@ import Card from "../../shared/components/UIElements/Card";
 import PlaceItem from "./PlaceItem";
 
 const PlaceList = (props) => {
-  if (props.items.lenght === 0) {
+  if (props.items.length === 0) {
     return (
       <div className="place-list center">
         <Card>
