fa8944f8445a93550b8eb6c949709c5e9a473b35
shayne243 <marques0@purdue.edu>
2020-08-06 16:15:51 +0400

    added google maps api

--- frontend/src/places/components/PlaceItem.js
+++ frontend/src/places/components/PlaceItem.js
@@ -4,6 +4,7 @@ import "./PlaceItem.css";
 import Card from "../../shared/components/UIElements/Card";
 import Button from "../../shared/components/FormElements/Button";
 import Modal from "../../shared/components/UIElements/Modal";
+import Map from "../../shared/components/UIElements/Map";
 
 const PlaceItem = (props) => {
   const [showMap, setShowMap] = useState(false);
@@ -20,7 +21,7 @@ const PlaceItem = (props) => {
         footer={<Button onClick={closeMapHandler}>CLOSE</Button>}
       >
         <div className="map-container">
-          <h2>THE MAP!</h2>
+          <Map center={props.coordinates} zoom={16} />
         </div>
       </Modal>
       <li className="place-item">
@@ -34,7 +35,9 @@ const PlaceItem = (props) => {
             <p>{props.description}</p>
           </div>
           <div className="place-item__actions">
-            <Button inverse onClick={openMapHandler}>VIEW ON MAP</Button>
+            <Button inverse onClick={openMapHandler}>
+              VIEW ON MAP
+            </Button>
             <Button to={`places/${props.id}`}>EDIT</Button>
             <Button danger>DELETE</Button>
           </div>
