88b481232495f2e7779016ee0f6b627b829e9557
shayne243 <marques0@purdue.edu>
2020-08-11 03:01:54 +0400

    post request to handle create place and uuid added

--- backend/controllers/places-controller.js
+++ backend/controllers/places-controller.js
@@ -1,5 +1,6 @@
+const { v4: uuid } = require("uuid");
+
 const HttpError = require("../models/http-error");
-const { get } = require("../routes/places-routes");
 
 const DUMMY_PLACES = [
   {
@@ -35,6 +36,22 @@ const getPlaceByUserId = (req, res, next) => {
   res.json({ place });
 };
 
+const createPlace = (req, res, next) => {
+  const { title, description, coordinates, address, creator } = req.body;
+  const createdPlace = {
+    id: uuid(),
+    title,
+    description,
+    location: coordinates,
+    address,
+    creator,
+  };
+
+  DUMMY_PLACES.push(createdPlace);
+
+  res.status(201).json({ place: createdPlace });
+};
 
 exports.getPlaceById = getPlaceById;
 exports.getPlaceByUserId = getPlaceByUserId;
+exports.createPlace = createPlace;
