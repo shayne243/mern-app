99898ba75e1b589948f3e5ad5a205b910315d1a4
shayne243 <marques0@purdue.edu>
2020-08-11 03:26:13 +0400

    finished places routes

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const placesRoutes = require("./routes/places-routes");
+const HttpError = require("./models/http-error");
 
 const app = express();
 
@@ -9,7 +10,13 @@ app.use(bodyParser.json());
 
 app.use("/api/places/", placesRoutes);
 
+app.use((req, res, next) => {
+  //unsupported routes
+  throw new HttpError("Could not find this route", 404);
+});
+
 app.use((error, req, res, next) => {
+  //default error handling
   if (res.headerSent) {
     return next(error);
   }
--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -5,8 +5,12 @@ const placesControllers = require("../controllers/places-controller");
 
 router.get("/:pid", placesControllers.getPlaceById);
 
-router.get("/user/:uid", placesControllers.getPlaceByUserId);
+router.get("/user/:uid", placesControllers.getPlacesByUserId);
 
 router.post("/", placesControllers.createPlace);
+
+router.patch("/:pid", placesControllers.updatePlaceById);
+
+router.delete("/:pid", placesControllers.deletePlace);
 
 module.exports = router;
